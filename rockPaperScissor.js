3545803733dcd62183e15d34d4b0b22e546bf00f
lpolverino <lorenz.polverino@gmail.com>
2023-01-25 15:48:14 -0300

    Added fn to get computer choice

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -1,1 +1,20 @@
-alert("Hola");
+/*
+a rock paper and scissor game against the machine
+*/
+const choices = ['rock', 'paper', 'scissors'];
+
+
+function getComputerChoice() {
+    /*
+    get the random value
+    log the awnser
+    and the return it 
+    */
+    let compChoice = Math.floor(Math.random() * 3);
+    console.log(compChoice);
+    compChoice = choices[compChoice];
+    console.log(compChoice);
+    return compChoice;
+}
+
+getComputerChoice();
