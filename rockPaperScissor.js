9d57122db824d48d76aa44329fb60fcca927e61a
lpolverino <lorenz.polverino@gmail.com>
2023-01-25 16:01:53 -0300

    Added function to play a round against the machine

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -1,15 +1,10 @@
 /*
 a rock paper and scissor game against the machine
 */
-const choices = ['rock', 'paper', 'scissors'];
+const choices = ["rock", "paper", "scissors"];
 
 
 function getComputerChoice() {
-    /*
-    get the random value
-    log the awnser
-    and the return it 
-    */
     let compChoice = Math.floor(Math.random() * 3);
     console.log(compChoice);
     compChoice = choices[compChoice];
@@ -17,4 +12,32 @@ function getComputerChoice() {
     return compChoice;
 }
 
-getComputerChoice();
+function playRound( playerChoice,computerChoice){
+    playerChoice = playerChoice.toLowerCase();
+    if(playerChoice ==="rock" && computerChoice !=="rock"){
+        if(computerChoice==="paper"){
+            return "You Lose!, Paper beats Rock"
+        }else{
+            return "You Win!, Rock beats Scissors"
+        }
+    }else if(playerChoice ==="paper" && computerChoice !=="paper"){
+        if(computerChoice==="scissors"){
+            return "You Lose!, Scissors beats Paper"
+        }else{
+            return "You Win!, Paper beats Rock"
+        }
+    }else if(playerChoice ==="scissors" && computerChoice !=="scissors"){
+        if(computerChoice==="rock"){
+            return "You Lose!, Rock beats Scissors"
+        }else{
+            return "You Win!, Scissors beats Paper"
+        }
+    }else{
+        return "DRAW!"
+    }
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
