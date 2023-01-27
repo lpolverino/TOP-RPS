3ca885cf8d81a521d579cda5365d6c088877af32
lpolverino <lorenz.polverino@gmail.com>
2023-01-27 12:20:25 -0300

    added functionality and ui interface

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -7,37 +7,49 @@ const choices = ["rock", "paper", "scissors"];
 function getComputerChoice() {
     let compChoice = Math.floor(Math.random() * 3);
     compChoice = choices[compChoice];
-    console.log(compChoice);
     return compChoice;
 }
 
 function playRound( playerChoice,computerChoice){
+    const results = document.querySelector('.results');
+    const marker = document.querySelector('#end-results');
+    marker.innerText = `${playerChoice} vs ${computerChoice}`;
+
+    const playerPar = document.querySelector('#player');
+    const computerPar = document.querySelector('#computer');
+
     playerChoice = playerChoice.toLowerCase();
     if(playerChoice ==="rock" && computerChoice !=="rock"){
         if(computerChoice==="paper"){
-            return 0
+            computerScore++;
         }else{
-            return 1
+            playerScore++;
         }
     }else if(playerChoice ==="paper" && computerChoice !=="paper"){
         if(computerChoice==="scissors"){
-            return 0
+            computerScore++;
         }else{
-            return 1
+            playerScore++;
         }
     }else if(playerChoice ==="scissors" && computerChoice !=="scissors"){
         if(computerChoice==="rock"){
-            return 0
+            computerScore++;
         }else{
-            return 1
+            playerScore++;
         }
-    }else{
-        return 0
     }
+    playerPar.innerText = playerPar.innerText.slice(0,-1) + playerScore;
+    computerPar.innerText = computerPar.innerText.slice(0, -1) + computerScore
 
+    //will not reset markers
+    if(playerScore === 5 || computerScore ===5){
+        const winner = document.createElement('p')
+        winner.innerText = `${playerScore === 5 ? "Player" : "Computer"} WIN'S!`
+        results.appendChild(winner);
+    }
 }
 
-function game(){
+/*function game(){
     let win_counts = 0;
     for(let i = 0; i < 5; i++){
         let playerSelection = prompt("Choince between: rock, paper, scissors")
@@ -52,4 +64,14 @@ function game(){
     }
 }
 
-game();
+game();*/
+
+const buttons = document.querySelectorAll('.btn');
+let playerScore = 0;
+let computerScore =0;
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => playRound(button.innerText,getComputerChoice()));
+}
+
+);
